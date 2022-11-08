1ff1cce77d8b7a3bb135c6e9a7036580d3c64f73
sumosh <s10223863@connect.np.edu.sg>
2022-11-08 14:52:44 +0800

    incorrect pin validated + OTP set up if > limit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Account Number Validation
 function validate() {
     if (document.myForm.acctnum.value == "" || isNaN(document.myForm.acctnum.value) ||
-        document.myForm.acctnum.value.length != 16) {
+        document.myForm.acctnum.value.length != 8) {
 
-        alert("Please provide an account number in the format #### #### #### ####.");
+        alert("Please provide an account number in the format #### ####.");
         document.myForm.acctnum.focus();
         return false;
     }
@@ -12,15 +12,32 @@ function validate() {
 
 //PIN Number Validation
 function validatePin() {
+    //if false
     if (document.PinForm.pinnum.value == "" || isNaN(document.PinForm.pinnum.value) ||
-        document.PinForm.pinnum.value.length != 6) {
-
+        document.PinForm.pinnum.value.length != 6) 
+    {
         alert("Please provide your 6-digit PIN number in the format ######.");
         window.location.assign("/incorrectPin.html")
         document.PinForm.pinnum.focus();
         return false;
     }
+    //if true
     return (true);
+}
+
+//if incorrect pin more than 3 times
+function incorrectPin(){
+    if((document.PinForm.pinnum.value.length == 6))
+    {
+        return true;
+    }
+    if((document.PinForm.pinnum.value == "" || isNaN(document.PinForm.pinnum.value) ||document.PinForm.pinnum.value.length != 6)>3)
+    {
+        window.location.assign("/blockCard.html");
+        return false;
+    }
+    window.location.assign("/blockCard.html");
+    return (false);
 }
 
 //tap anywhere on screen to begin
@@ -28,4 +45,16 @@ function clickFunction() {
     document.getElementById("click").onclick = function() {
         location.href = "/index.html";
     };
+}
+
+//$(":input").inputmask();
+
+//withdraw an amount
+function withdrawAmt(){
+    if(document.withdrawForm.amt.value > 500)
+    {
+        window.location.assign('/otp.html')
+        return (false);
+    }    
+    return (true);
 }
